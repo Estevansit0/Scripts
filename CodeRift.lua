68bb31329b35d4ad26508225a223efa637beec23
Estevansito <160373638+CodeRiftH@users.noreply.github.com>
2024-08-21 09:21:48 -0700

    Update CodeRift.lua

--- CodeRift.lua
+++ CodeRift.lua
@@ -4,4 +4,7 @@ local GameName = game:GetService("MarketplaceService"):GetProductInfo(game.Place
 if Place == 15705682243 then
     warn(GameName)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CodeRiftH/Scripts/main/Games/TLF.lua"))()
+elseif Place == 11445923563 then
+    warn(GameName)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CodeRiftH/Scripts/main/Games/OFS.lua"))()
 end
