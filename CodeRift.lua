ea77646b98c2d6724c0a5d0770213329837465e7
Estevansito <160373638+Estevansit0@users.noreply.github.com>
2024-11-15 17:23:26 -0800

    Update CodeRift.lua

--- CodeRift.lua
+++ CodeRift.lua
@@ -3,14 +3,14 @@ local Game = game.GameId
 local GameName = game:GetService("MarketplaceService"):GetProductInfo(Place).Name
 
 local scripts = {
-    [15705682243] = "https://raw.githubusercontent.com/CodeRiftH/Scripts/main/Games/TLF.lua",
-    [18463280776] = "https://raw.githubusercontent.com/CodeRiftH/Scripts/main/Games/OPFX.lua",
-    [17824828446] = "https://raw.githubusercontent.com/CodeRiftH/Scripts/main/Games/SCX.lua",
-    [7903991471] = "https://raw.githubusercontent.com/CodeRiftH/Scripts/main/Games/PT.lua",
-    [15501353806] = "https://raw.githubusercontent.com/CodeRiftH/Scripts/main/Games/SMS.lua",
-    [8069117419] = "https://raw.githubusercontent.com/CodeRiftH/Scripts/main/Games/DSS.lua",
-    [4161970303] = "https://raw.githubusercontent.com/CodeRiftH/Scripts/main/Games/AS.lua",
-    [4069560710] = "https://raw.githubusercontent.com/CodeRiftH/Scripts/main/Games/OFS.lua"
+    [15705682243] = "https://raw.githubusercontent.com/Estevansit0/Scripts/main/Games/TLF.lua",
+    [18463280776] = "https://raw.githubusercontent.com/Estevansit0/Scripts/main/Games/OPFX.lua",
+    [17824828446] = "https://raw.githubusercontent.com/Estevansit0/Scripts/main/Games/SCX.lua",
+    [7903991471] = "https://raw.githubusercontent.com/Estevansit0/Scripts/main/Games/PT.lua",
+    [15501353806] = "https://raw.githubusercontent.com/Estevansit0/Scripts/main/Games/SMS.lua",
+    [8069117419] = "https://raw.githubusercontent.com/Estevansit0/Scripts/main/Games/DSS.lua",
+    [4161970303] = "https://raw.githubusercontent.com/Estevansit0/Scripts/main/Games/AS.lua",
+    [4069560710] = "https://raw.githubusercontent.com/Estevansit0/Scripts/main/Games/OFS.lua"
 }
 
 local scriptURL = scripts[Place] or scripts[Game]
