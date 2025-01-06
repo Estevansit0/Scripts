e5ab645990116e95c0e1f6cd0589498412c7a2c1
Estevansito <160373638+Estevansit0@users.noreply.github.com>
2025-01-06 10:36:32 -0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -5,12 +5,9 @@ local GameName = game:GetService("MarketplaceService"):GetProductInfo(game.Place
 if Game == 18463280776 then
     warn(GameName)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Estevansit0/Scripts/main/Games/OPFX.lua"))()
-elseif Place == 11756036029 or Place == 73417524077325 or Place == 16041086429 then
+elseif Game == 4161970303 then
     warn(GameName)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Estevansit0/Scripts/main/Games/AS.lua"))()
-elseif Place ~= 11756036029 and Place ~= 73417524077325 and Place ~= 16041086429 and Game == 4161970303 then
-    warn(GameName)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Estevansit0/Scripts/refs/heads/main/Games/AsDungeon.lua"))()
 elseif Game == 3808223175 then
     warn(GameName)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Estevansit0/Scripts/refs/heads/main/Games/JJIF.lua"))()
