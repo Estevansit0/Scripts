2f23a72d975d58a2294046321b0338504e58f5e1
Estevansito <160373638+Estevansit0@users.noreply.github.com>
2025-01-15 19:47:47 -0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -15,6 +15,8 @@ elseif Game == 6664476231 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Estevansit0/Scripts/refs/heads/main/Games/FR.lua"))()
 elseif Game == 1119466531 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Estevansit0/Scripts/refs/heads/main/Games/LOFS.lua"))()
+elseif Game == 6982846329 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Estevansit0/Scripts/refs/heads/main/Games/PSS.lua"))()
 elseif Place == 3956818381 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Estevansit0/Scripts/refs/heads/main/Games/NL.lua"))()
 else
