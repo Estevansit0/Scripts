6a7e6eeb69b4cb6dd6aae356974aa23866a1c686
Estevansito <160373638+Estevansit0@users.noreply.github.com>
2025-03-14 17:45:15 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -66,6 +66,7 @@ local scripts = {
         [75959166903570] = "https://raw.githubusercontent.com/Estevansit0/Scripts/refs/heads/main/Games/FrDng.lua",
         [80157158224004] = "https://raw.githubusercontent.com/Estevansit0/Scripts/refs/heads/main/Games/FrDng.lua",
         [126000682773050] = "https://raw.githubusercontent.com/Estevansit0/Scripts/refs/heads/main/Games/FrDng.lua",
+        [139511259501829] = "https://raw.githubusercontent.com/Estevansit0/Scripts/refs/heads/main/Games/FrLobby.lua",
         default = "https://raw.githubusercontent.com/Estevansit0/Scripts/refs/heads/main/Games/FR.lua"
     },
     [1119466531] = "https://raw.githubusercontent.com/Estevansit0/Scripts/refs/heads/main/Games/LOFS.lua",
