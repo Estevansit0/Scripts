327f8452e327246e41027c1679c8cad30a385dd8
Estevansito <160373638+Estevansit0@users.noreply.github.com>
2025-03-18 20:45:25 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -12,12 +12,21 @@ if not game:IsLoaded() then
 end
 
 _G.Scripts = {
-    [6471449680] = "dc97e745159ff7cc618d2bbf720e02b2",
-    [6664476231] = "76c26e15de37a6a72e7f43351d10a76a",
-    [4161970303] = "8eef70cee3e0024fc33be2214e5ae0b9",
-    [4069560710] = "0ff8c46eb6407e489866d8ab854438eb",
-    [6793832056] = "184b79224c9d4809d72b34a8c2d3c815",
-    [1119466531] = "https://raw.githubusercontent.com/Estevansit0/Scripts/refs/heads/main/Games/LOFS.lua",  
+    [6471449680] = {
+        [86639052909924] = "dc97e745159ff7cc618d2bbf720e02b2",
+        default = "19c955ebde5dbd97b3519eb4cc66aba2"
+    },
+    [4069560710] = {
+        [17333805250] = "0ff8c46eb6407e489866d8ab854438eb",
+        default = "910c85a823813ca236ce92792a75f991"
+    },
+    [6664476231] = {
+        [75959166903570] = "1407880ae6f399130b783d99080b2b4b",
+        [80157158224004] = "1407880ae6f399130b783d99080b2b4b",
+        [126000682773050] = "1407880ae6f399130b783d99080b2b4b",
+        default = "76c26e15de37a6a72e7f43351d10a76a"
+    },
+    [1119466531] = "https://raw.githubusercontent.com/Estevansit0/Scripts/refs/heads/main/Games/LOFS.lua",
     [3956818381] = "https://raw.githubusercontent.com/Estevansit0/Scripts/refs/heads/main/Games/NL.lua",
     [6256925440] = "https://raw.githubusercontent.com/Estevansit0/Scripts/main/Games/OPFX.lua",
     [6982846329] = "https://raw.githubusercontent.com/Estevansit0/Scripts/refs/heads/main/Games/PSS.lua",
@@ -27,7 +36,7 @@ _G.Scripts = {
 if not _G.Override then
     _G.Override = {}
 end
-for i,v in pairs(_G.Override) do
+for i, v in pairs(_G.Override) do
     _G.Scripts[i] = v
 end
 
@@ -52,19 +61,27 @@ if request then
     end)
 end
 
-local gameId = game.gameId
+local gameId = game.GameId
+local placeId = game.PlaceId
+local scriptSource
+
 if _G.Scripts[gameId] then
-    local scriptSource = _G.Scripts[gameId]
-    if type(scriptSource) == "string" and scriptSource:sub(1, 8) == "https://" then
-        loadstring(game:HttpGet(scriptSource))()
-        return
+    if type(_G.Scripts[gameId]) == "table" then
+        scriptSource = _G.Scripts[gameId][placeId] or _G.Scripts[gameId].default
+    else
+        scriptSource = _G.Scripts[gameId]
     end
+end
+
+if type(scriptSource) == "string" and scriptSource:sub(1, 8) == "https://" then
+    loadstring(game:HttpGet(scriptSource, true))()
+    return
 end
 
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Estevansit0/Scripts/refs/heads/main/KeySystemUi/luarmor.lua"))()
 local Window = Library:Window({
     DisplayName = "QuantumPulsar X",
-    script_id = _G.Scripts[game.gameId],
+    script_id = scriptSource,
     File = "PulsarKey.txt",
     Discord = "discord.gg/PulsarX",
     MinIcon = "rbxassetid://100569530935041",
